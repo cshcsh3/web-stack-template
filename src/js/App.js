11fc0c41be4b67f0e187137f4dee79eacd2a5b96
shanhan <thecsh3@gmail.com>
2021-03-18 18:07:32 +0800

    Init project

---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../css/base.scss'
+
+function App () {
+  return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+  )
+}
+
+export default App
